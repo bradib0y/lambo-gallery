51ec731c7fcb50454d3ee58c21b154279c8136d7
Balázs Pukli <balazsptm@gmail.com>
2017-08-07 12:37:29 +0200

    Lambo Model class added

---
+++ src/LamboGallery/Models/Lambo.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LamboGallery.Models
+{
+    public class Lambo
+    {
+        public int Id { get; set; }
+        public string Type { get; set; }
+        public string Variety { get; set; }
+        public string Description { get; set; }
+        public string[] Properties { get; set; }
+        public bool Favorite { get; set; }
+
+        public string DisplayText {
+            get { return Type + " " + Variety; }
+        }
+
+        public string CoverImageFileName {
+            get { return Type.Replace(" ", "-").ToLower() + "-" + Variety.Replace(" ", "-") + ".jpg"; }
+        }
+    }
+}
